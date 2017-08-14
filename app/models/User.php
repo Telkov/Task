3d42aef107c4a3fd708eb6ecb645380f20815764
PR1ME <sanya_fl@ukr.net>
2017-08-14 21:54:11 +0300

    2

--- app/models/User.php
+++ app/models/User.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alejandro
- * Date: 14.08.2017
- * Time: 15:39
- */
 
 namespace app\models;
 
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
 <?php
 
 require_once('vendor/autoload.php');
@@ -10,8 +23,39 @@ try {
 //    $resInsert = $DB->get_row("SELECT * FROM forTest WHERE id = 3");
     $resInsert = $DB->get_rows("SELECT * FROM forTest");
 
-    print_r($resInsert);
+    echo '<ul>';
+
+    echo '</ul>';
+//    echo '<pre>';
+//    print_r($resInsert);
+//    echo '</pre>';
+
 }
 catch (Exception $e) {
     echo $e->getMessage();
 }
+?>
+
+<table class="table table-primary">
+    <tr>
+        <th>title</th>
+        <th>date</th>
+        <th>author</th>
+    </tr>
+
+    <?php
+    foreach ($resInsert as $value) {
+    echo '<tr>';
+    echo '<td>' . $value['title'] . '</td>';
+    echo '<td>' . $value['date'] . '</td>';
+    echo '</tr>';
+    }
+
+    ?>
+
+</table>
+
+
+</body>
+</html>
+
